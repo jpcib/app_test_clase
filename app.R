65fb67472fb0fa4cbcdc7f4b40ea1067a871253c
Juan Cibantos <jpcibantos@gmail.com>
2025-11-26 09:36:56 -0300

    Cambio n bins

--- app.R
+++ app.R
@@ -21,8 +21,8 @@ ui <- fluidPage(
             sliderInput("bins",
                         "Number of bins:",
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 65,
+                        value = 33)
         ),
 
         # Show a plot of the generated distribution
