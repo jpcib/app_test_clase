8113182aadf06d3f49f7cf308f63e407e64caba6
Juan Cibantos <jpcibantos@gmail.com>
2025-11-26 09:31:32 -0300

    Inicio traducción

--- app.R
+++ app.R
@@ -13,13 +13,13 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("La vieja y confiable data de géiseres"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             sliderInput("bins",
-                        "Number of bins:",
+                        "Número de bins:",
                         min = 1,
                         max = 50,
                         value = 30)
